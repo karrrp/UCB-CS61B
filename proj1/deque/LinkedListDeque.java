c29b1fb84e47a97b990bd83fdfcb6e2907b4c942
karup <2508656934@qq.com>
2024-09-19 19:42:44 +0800

    640

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -147,7 +147,7 @@ public class LinkedListDeque<T> implements Deque<T>, Iterable<T> {
             Deque<T> o1 = (Deque<T>) o;
             if (this.size() == o1.size()) {
                 for (int i = 0; i < o1.size(); i++) {
-                    if (o1.get(i) != ((Deque<?>) o).get(i)) {
+                    if (o1.get(i) != this.get(i)) {
                         return false;
                     }
                 }
